eb7648f4b8f90c31d4cfc5cad1c5465cc018e88c
Evan Miller <ewmst5@gmail.com>
2025-05-26 09:41:01 -0400

    adding html structure and .js framework

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// const form = document.getElementById('journal-form');
+// const entriesDiv = document.getElementById('entries');
+
+// form.addEventListener('submit', function(e) {
+//   e.preventDefault();
+  
+//   const title = document.getElementById('entry-title').value;
+//   const content = document.getElementById('entry-content').value;
+
+//   const entryEl = document.createElement('div');
+//   entryEl.classList.add('entry');
+//   entryEl.innerHTML = `
+//     <h3>${title}</h3>
+//     <p>${content}</p>
+//   `;
+
+//   entriesDiv.prepend(entryEl);
+
+//   form.reset();
+// });
+cd 
